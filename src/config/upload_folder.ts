e99c415be13bca0173ea6acd6cb5f61d30c530a9
GFSprog40 <gustavo.prog40@gmail.com>
2023-06-24 16:04:57 -0300

    feito imagem uploads

--- src/config/upload_folder.ts
+++ src/config/upload_folder.ts
@@ -6,9 +6,10 @@ const uploadConfig = {
   storage: multer.diskStorage({
     destination: path.resolve(__dirname, '..', '..', 'uploads'),
     filename(_req, file, cb) {
-      const [name] = file.originalname.split('.')
-      const filename = `${name}.jpg`
-      cb(null, filename)
+      // const [name] = file.originalname.split('.')
+      // const filename = `${name}.jpg`
+      // cb(null, filename)
+      cb(null, file.originalname)
     },
   }),
 }
