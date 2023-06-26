9723f07c7fdd8d5bec322a9c67c738f969226ed6
GFSprog40 <gustavo.prog40@gmail.com>
2023-06-26 13:22:24 -0300

    image files

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ app.use(cors({
 app.use(express.json())
 app.use(routes)
 
-app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
+app.use('/files', express.static(path.resolve(__dirname, '..', './src/uploads')))
 
 app.listen(PORT, () => {
   console.log(` App Running: ${PORT}`)
