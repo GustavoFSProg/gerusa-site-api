f5d64d6afa6613d4b5a558d9907f1dbd3853c6c4
GFSprog40 <gustavo.prog40@gmail.com>
2023-06-26 12:23:08 -0300

    files

--- index.ts
+++ index.ts
@@ -24,7 +24,7 @@ app.use(express.json())
 app.use(routes)
 
 
-// app.use('/files', express.static(path.resolve(__dirname, '..', 'src/uploads')))
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.listen(PORT, () => {
   console.log(` App Running: ${PORT}`)
